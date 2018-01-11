59f9e865e7606e21d804d45d5bd5e5f0301b5b4e
Olivia29 <15712874159@163.com>
2018-01-11 17:11:50 +0800

    node.js and express

--- demo-server/client.js
+++ demo-server/client.js
@@ -1,6 +1,6 @@
 let http = require('http');
 let util = require('util');
-let jsonp = require('jsonp')
+// let jsonp = require('jsonp')
 http.get('http://www.imooc.com/u/card',function(res){
     let data = '';
     res.on('data',function(chunk){
